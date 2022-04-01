07da485e63b8c956ee21e6125b0ad1ea824c7e2a
kamran khan <77343380+3kdeveloper@users.noreply.github.com>
2022-04-01 09:27:18 +0500

    creating item model

--- lib/model/item_model.dart
+++ lib/model/item_model.dart
@@ -1,1 +1,15 @@
+class ItemModel {
+  final String? imagePath;
+  final String? title;
+  final String? description;
+  final String? ingredients;
+  final double? price;
 
+  ItemModel({
+    this.imagePath,
+    this.title,
+    this.description,
+    this.ingredients,
+    this.price,
+  });
+}
